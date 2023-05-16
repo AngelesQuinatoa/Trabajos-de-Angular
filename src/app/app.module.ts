bf19e819623d149d250b15c911a3ef4da17495f9
ANGELES NAGELY QUINATOA SANCHEZ <97192998+AngelesQuinatoa@users.noreply.github.com>
2023-05-15 19:33:04 -0500

    Angelescambios2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PagesModule } from './pages/pages.module';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { FooterComponent } from './shared/footer/footer.component';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
     declarations: [
-        AppComponent, 
+        AppComponent
     ],
     providers: [],
     bootstrap: [AppComponent],
     imports: [
         BrowserModule,
         AppRoutingModule,
-        PagesModule
+        PagesModule,
+        HttpClientModule
+
     ]
 })
 export class AppModule { }
